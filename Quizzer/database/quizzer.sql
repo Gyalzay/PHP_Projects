84596797e36e6e8f2dd3361d26125e9ae23ce5c7
Gyalzay <45729329+Gyalzay@users.noreply.github.com>
2018-12-09 17:07:31 +0545

    Add files via upload

---
+++ Quizzer/database/quizzer.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 09, 2018 at 12:17 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quizzer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `choices`
+--
+
+CREATE TABLE `choices` (
+  `id` int(11) NOT NULL,
+  `question_number` int(11) NOT NULL,
+  `is_correct` tinyint(1) NOT NULL DEFAULT '0',
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `choices`
+--
+
+INSERT INTO `choices` (`id`, `question_number`, `is_correct`, `text`) VALUES
+(1, 1, 0, 'Private Home Page'),
+(2, 1, 0, 'Personal Home Page'),
+(3, 1, 1, 'PHP Hypertext Preprocessor'),
+(4, 1, 0, 'Personal Hypertext Preprocessor'),
+(5, 2, 1, 'echo \"Hello World\";'),
+(6, 2, 0, '\"Hello World\";'),
+(7, 2, 0, 'System.out.println(\"Hello World\");'),
+(8, 2, 0, 'Document.Write(\"Hello World\");'),
+(9, 3, 0, 'ASP'),
+(10, 3, 0, 'RUBY'),
+(11, 3, 1, 'PHP'),
+(12, 4, 0, '$_GET'),
+(13, 4, 1, '$_SERVER'),
+(14, 4, 0, '$_SESSION'),
+(15, 4, 0, '$_GLOBALS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `question_number` int(11) NOT NULL,
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`question_number`, `text`) VALUES
+(1, 'What does PHP stand for?'),
+(2, 'How do you write \"Hello World\" in PHP'),
+(3, 'What is the best server side langugage?'),
+(4, 'Which superglobal variable holds information about headers, paths, and script locations?');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `choices`
+--
+ALTER TABLE `choices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`question_number`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `choices`
+--
+ALTER TABLE `choices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
